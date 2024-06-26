dad001e870e7f8d5a5ccdc658eb5b264abc27a4e
code-iddih <idinyamwaya@gmail.com>
2024-06-26 12:32:25 +0300

    Completing thhe Tasks

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function Expression *Divide*
+
+function divide (a = "2000",b = "100") {
+
+    return a / b ;
+}
+
+// Arrow function *Square*
+
+const square = parS => parS ** 2;
+
+// Arrow function *Add*
+
+const add = (parA, parB) => {return parA + parB; } 
